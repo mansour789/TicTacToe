6417f0a59dd212bbc9fd4444ebc808ddacb0f30e
mansour789 <m0509111136@gmail.com>
2019-07-03 21:44:51 +0300

    add some comments

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const start = performance.now();
+
 //== the main array==//
 let bord = [
     [0, 0, 0],
@@ -39,26 +39,14 @@ const playBtnC = document.querySelector('.playBtnC')
 
 //==scoring title==//
 const score = document.querySelector('.score');
-//==store the cat and viking score===//
+//==saving the cat and viking score===//
 let catScore = document.querySelector('#catScore');
 let vikingScore = document.querySelector('#vikingScore');
 
 
-//==storing parent div==//
+//==storing big div that have all small divs in it==//
 const board = document.querySelector('#board');
-//==storing div's children of the parent div==//
-const q1 = document.querySelector('#n00');
-const q2 = document.querySelector('#n01');
-const q3 = document.querySelector('#n02');
-
-const q4 = document.querySelector('#n10');
-const q5 = document.querySelector('#n11');
-const q6 = document.querySelector('#n12');
-
-const q7 = document.querySelector('#n20');
-const q8 = document.querySelector('#n21');
-const q9 = document.querySelector('#n22');
-
+//==GIF of cat dance when the cat win===//
 const catDance = document.querySelector('#dance');
 
 
@@ -76,7 +64,7 @@ const startGame = function () {
     intro.setAttribute('src', 'audio/startGame.mp3');
     //==remove the score from the screen==//
     score.style.display = 'none';
-    
+
 }
 
 //==when click playAgain button==//
@@ -84,8 +72,9 @@ const restart = function () {
     window.location.reload(true);
 }
 
-//==check of AI mode working===//
+//==check of AoutPlay mode working===//
 let AI = false;
+//==counting how many cat pressd , use this in AuotoPlay mode===//
 let conter = 0;
 //===var for swiching turn==//
 let playerTurn = true;
@@ -117,19 +106,22 @@ const drop = function () {
                     if (bord[i][j] === 0) {
                         //==change the value of the element in the main array to be 'C'// 
                         bord[i][j] = "C";
-                        //==set the image background to the div ===//
+                        //==set the background image  of the div ===//
                         image.setAttribute('src', 'images/cat.gif');
-
+                        //==counting how many cat play , for the autoPlay mode===//
                         conter++;
-                        //==calling the clicking function==//
-                        if(!AI){
-                        clicking(cat, false);
-                          }else{
-                            clicking(cat, true) ;
+                        //===check if the autoPlay mode is false==//
+                        if (!AI) {
+                            //==calling the clicking function with (playerTurn) value of 'false'==//
+                            clicking(cat, false);
+                        } else {
+                            //==calling the clicking functionwith (playerTurn) value of 'true'==//
+                            clicking(cat, true);
+                            //==calling the autoPlay function==//
                             aifunction();
-                          }
-                          
-                        
+                        }
+
+
                     }//<--end of Cat turn==//
 
                 } else { //==if(playerTurn = false) means viking turn==//
@@ -138,7 +130,7 @@ const drop = function () {
                         //==change the value of the element in the main array to be 'V'// 
                         bord[i][j] = "V";
                         //==calling the clicking function==//
-                        clicking(sword, true );
+                        clicking(sword, true);
                         //==set the image background to the div ===//
                         image.setAttribute('src', 'images/vikings.gif');
                     }
@@ -171,23 +163,22 @@ const winGame = function () {
     ) {
 
         //==check vikings win===//
-        // if (whoPlaying === 'V') {
+       
+        //== set opcity of 'X,O' table to 0.6=====//
+        board.style.opacity = '0.6';
+        //==calling win theme function===//
+        winTheme(swordLast, `Viking has skined another cat`, "#FFF");
+        //===calling the fadeOut function ===//
+        setTimeout(fadeOut, 100);
+        //==calling score function==;;
+        scoring('scoreSavedV');
+        //==calling play again function==//
+        playAgain();
+        return;
+    }//<--end of check viking win ==//
 
-            //== set opcity of 'X,O' table to 0.6=====//
-            board.style.opacity = '0.6';
-            //==winning theme function===//
-            winTheme(swordLast, `Viking has skined another cat`, "#FFF");
-            //===calling the fadeOut function ===//
-            setTimeout(fadeOut, 100);
-            //==Adding score function==;;
-            scoring('scoreSavedV');
-             //==calling play again function==//
-             playAgain();
-             return;
-         }//<--end of check viking win ==//
-
-        //==if cat win===//
-        // if (whoPlaying === 'C') {
+    //==if cat win===//
+    
     if ((bord[0][0] === "C" && bord[0][0] === bord[0][1] && bord[0][0] === bord[0][2]) ||
         (bord[1][0] === "C" && bord[1][0] === bord[1][1] && bord[1][0] === bord[1][2]) ||
         (bord[2][0] === "C" && bord[2][0] === bord[2][1] && bord[2][0] === bord[2][2]) ||
@@ -200,31 +191,26 @@ const winGame = function () {
         (bord[0][2] === "C" && bord[0][2] === bord[1][1] && bord[0][2] === bord[2][0])
 
     ) {
-            //==winning theme function===//
-            winTheme(catLast, `one more cat just survived`, 'red');
-            //===cahnge background image and edit the style====//
-            document.querySelector('body').style.background = 'url(images/catWin.jpg) no-repeat center center fixed';
-            document.querySelector('body').style.backgroundSize = 'cover';
-            catDance.style.display = 'block';
-            //===stope the background music====//
-            intro.setAttribute('src', ' ');
-            //==Adding score==//
-            scoring('scoreSavedC');
-             //==calling play again function==//
-             playAgain();
-             return;
-         }//<--end of check cat win==//
-
-       
-
-
-    // }//<--end of checking all caces of win==// 
+        //==calling win theme function===//
+        winTheme(catLast, `one more cat just survived`, 'red');
+        //===cahnge background image and edit the style====//
+        document.querySelector('body').style.background = 'url(images/catWin.jpg) no-repeat center center fixed';
+        document.querySelector('body').style.backgroundSize = 'cover';
+        catDance.style.display = 'block';
+        //===stope the background music==  <<--This is bad solution Hope to fix it soon//
+        intro.setAttribute('src', ' ');
+        //==Adding score==//
+        scoring('scoreSavedC');
+        //==calling play again function==//
+        playAgain();
+        return;
+    }//<--end of check cat win==// 
 
     //==check if the game is Draw==//
 
     if (bord[0][0] && bord[0][1] && bord[0][2] &&
         bord[1][0] && bord[1][1] && bord[1][2] &&
-        bord[2][0] && bord[2][1] && bord[2][2] ) {
+        bord[2][0] && bord[2][1] && bord[2][2]) {
         //===remove title===// 
         header.style.display = 'none';
         //===change text & color of win message then display it==//
@@ -236,12 +222,12 @@ const winGame = function () {
         return;
 
     }//<--end of draw checking====//
-    
+
 } //<-- end of the win function===//
 
 
-//====drawing image and play sound when clicked===//
-const clicking = function (sond, player, val) {
+//====play sound when clicked && and change (playerTurn) value ===//
+const clicking = function (sond, player) {
     //==play sound effect==//
     sond.play();
     //==swich turn==//
@@ -262,9 +248,6 @@ const winTheme = function (sond, inText, color) {
     //==change color for win message==//
     win.style.color = color;
 }
-
-
-
 
 //=====This fuction to let the blood fade out When Viking win===//
 const fadeOut = function () {
@@ -297,9 +280,9 @@ const scoring = function (whoScore) {
         sessionStorage.setItem(whoScore, '1');
     } else {
         //==if there is a saved score get the score and add 1 to it====//
-        let VS = sessionStorage.getItem(whoScore);
-        let VI = parseInt(VS) + 1;
-        let scorGet = sessionStorage.setItem(whoScore, VI.toString());
+        let scoreString = sessionStorage.getItem(whoScore);
+        let scoreIntger = parseInt(scoreString) + 1;
+        let scorGet = sessionStorage.setItem(whoScore, scoreIntger.toString());
         return scorGet;
     }
 
@@ -317,34 +300,7 @@ const playAgain = function () {
 }
 
 
-//==playing with the jenes===//
-const jenes = function(){
-    let jene = document.querySelectorAll('.jene');
-    
-    for (let i = 0; i < jene.length; i++) {
-        let random1 = Math.floor(Math.random() * 70);     
-         let random2 = Math.floor(Math.random() * 70);
-
-        jene[i].style.display = 'block';
-        jene[i].style.top = `${random1}%`;
-        jene[i].style.left = `${random2}%`;
-        jene[i].style.width = `${random2}%`;
-        jene[i].style.height = `${random1}%`
-
-        
-    }
-}
-
-
-
-
-
-
-
-
-
-//======Ceating AI ;) ======//
-
+//======Ceating AutoPlay Mode ;) ======//
 
 const aifunction = function () {
     // console.log('ffdsf');
@@ -357,18 +313,18 @@ const aifunction = function () {
     if (bord[random1][random2] === 0) {
         bord[random1][random2] = "V";
         clicking(sword, true);
-        imageq.setAttribute('src', 'images/vikings.gif');  
+        imageq.setAttribute('src', 'images/vikings.gif');
     } else {
-        
-        if(conter < 5){
-        aifunction();
+
+        if (conter < 5) {
+            aifunction();
         }
     }
 }
 
 
-//==starting the AI mode function==//
-const startAI = function(){
+//==starting the AutoPlay mode function when AutoPlay button click==//
+const startAI = function () {
     AI = true;
     startGame();
 }
@@ -380,27 +336,8 @@ playBtnC.addEventListener('click', startAI);
 
 //==adding click event to the div's==//
 
+let box = document.querySelectorAll('.box');
 
-q1.addEventListener('click', drop);
-q2.addEventListener('click', drop);
-q3.addEventListener('click', drop);
-q4.addEventListener('click', drop);
-q5.addEventListener('click', drop);
-q6.addEventListener('click', drop);
-q7.addEventListener('click', drop);
-q8.addEventListener('click', drop);
-q9.addEventListener('click', drop);
-
-
-
-const end = performance.now();
-
-console.log(end - start);
-
-
-
-
-
-// bord[0][0] && bord[0][1] && bord[0][2] &&
-//         bord[1][0] && bord[1][1] && bord[1][2] &&
-//         bord[2][0] && bord[2][1] && bord[2][2] !== 0
+    for (let i = 0; i < box.length; i++) {
+        box[i].addEventListener('click', drop);
+    }
